ef8cc44812efa5c237473e9efe32e6da933d0264
Travis Scott <rafinha57009125@gmail.com>
2024-11-18 20:55:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'quem é o melhor cantor?',
+    'Travis Scott ou Taylor swift',
+    'Travis'
+)
+
+criaCartao(
+    'quem é o melhor jogador de basquete?',
+    'Lebrom james ou Michael Jordan?',
+    'Michael Jordan'
+)
+
+criaCartao(
+    'quem é o melhor jogador?',
+    'Messi ou CR7?',
+    'cr7'
+)
+
+criaCartao(
+    'Quem é o maior artista de todos os tempos?',
+    'Michel Jackson ou Freddie Mercury',
+    'Michael Jackson'
+)
